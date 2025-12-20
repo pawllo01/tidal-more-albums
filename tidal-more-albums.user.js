eb4a397fea2cdc5b98503dadc44e5b433d2ee8cd
Paweł Januszek <99902341+pawllo01@users.noreply.github.com>
2025-12-20 14:44:31 +0100

    1.1 - fix & add custom links

--- tidal-more-albums.user.js
+++ tidal-more-albums.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Tidal More Albums
 // @namespace    https://github.com/pawllo01/tidal-more-albums
-// @version      1.0
+// @version      1.1
 // @description  Show all releases in artists' discographies.
 // @author       pawllo01
-// @match        *://listen.tidal.com/*
+// @match        https://tidal.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tidal.com
 // @grant        unsafeWindow
 // ==/UserScript==
@@ -12,11 +12,8 @@
 (function () {
   'use strict';
 
-  // store the original fetch function
   const originalFetch = unsafeWindow.fetch;
 
-  // override the fetch function
-  // https://developer.mozilla.org/en-US/docs/Web/API/Window/fetch
   unsafeWindow.fetch = function (resource, options) {
     // check if the request URL matches
     if (
@@ -27,27 +24,28 @@
         // clone the response to read and modify the body
         const clonedResponse = response.clone();
 
-        return clonedResponse.json().then((data) => {
-          const url = window.location.href;
+        return clonedResponse
+          .json()
+          .then((data) => {
+            const url = location.href;
 
-          let filter;
-          if (url.includes('a4f964ba-b52e-41e8-b25c-06cd70c1efad')) filter = 'ALBUMS';
-          else if (url.includes('65f09547-3bba-4764-ad0c-8e105f776fa7')) filter = 'EPSANDSINGLES';
-          else return response;
+            let filter;
+            if (url.includes('a4f964ba-b52e-41e8-b25c-06cd70c1efad')) filter = 'ALBUMS';
+            else if (url.includes('65f09547-3bba-4764-ad0c-8e105f776fa7')) filter = 'EPSANDSINGLES';
+            else return response;
 
-          // override dataApiPath
-          const artistId = getArtistId(url);
-          data.rows[0].modules[0].pagedList.dataApiPath = `artists/${artistId}/albums?filter=${filter}`;
+            const artistId = getArtistIdFromUrl(url);
+            data.rows[0].modules[0].pagedList.dataApiPath = `artists/${artistId}/albums?filter=${filter}`;
+            data.rows[0].modules[0].pagedList.items = [];
 
-          // return the new response with the modified data
-          const modifiedResponse = new Response(JSON.stringify(data), {
-            status: clonedResponse.status,
-            statusText: clonedResponse.statusText,
-            headers: clonedResponse.headers,
-          });
-
-          return modifiedResponse;
-        });
+            // return the new response with the modified data
+            return new Response(JSON.stringify(data), {
+              status: response.status,
+              statusText: response.statusText,
+              headers: response.headers,
+            });
+          })
+          .catch(() => response);
       });
     }
 
@@ -55,8 +53,68 @@
     return originalFetch(resource, options);
   };
 
-  function getArtistId(url) {
-    const urlParams = new URLSearchParams(url.split('?')[1]);
-    return urlParams.get('artistId');
+  function getArtistIdFromUrl(url) {
+    // e.g. https://tidal.com/artist/7162333
+    const pathMatch = url.match(/\/artist\/([^\/\?]+)/);
+    if (pathMatch) return pathMatch[1];
+
+    // e.g. https://tidal.com/view/pages/single-module-page/ae223310-a4c2-4568-a770-ffef70344441/4/a4f964ba-b52e-41e8-b25c-06cd70c1efad/2?artistId=7162333
+    const params = new URLSearchParams(url.split('?')[1]);
+    return params.get('artistId');
   }
+
+  function addCustomLinks() {
+    function insertLinks() {
+      const artistId = getArtistIdFromUrl(location.href);
+      if (!artistId) return;
+
+      const containers = document.querySelectorAll('div._buttonContainer_a150d77');
+
+      containers.forEach((container) => {
+        if (container.querySelector('a.custom-link')) return; // skip if custom link was added
+
+        const originalLink = container.querySelector('a[data-test="view-all-link"]');
+        const href = originalLink?.href || '';
+
+        if (href.includes('ARTIST_ALBUMS')) {
+          container.append(createLink('albums', artistId));
+        } else if (href.includes('ARTIST_TOP_SINGLES')) {
+          container.append(createLink('singles', artistId));
+        }
+      });
+    }
+
+    function createLink(type, artistId) {
+      const DISCOGRAPHY_PAGE_BASE =
+        '/view/pages/single-module-page/ae223310-a4c2-4568-a770-ffef70344441/4/';
+      const MODULES = {
+        albums: 'a4f964ba-b52e-41e8-b25c-06cd70c1efad/2',
+        singles: '65f09547-3bba-4764-ad0c-8e105f776fa7/1',
+      };
+
+      const href = `${DISCOGRAPHY_PAGE_BASE}${MODULES[type]}?artistId=${artistId}`;
+
+      const customLink = document.createElement('a');
+      customLink.className = '_viewAllButton_4b4f90c custom-link';
+      customLink.setAttribute('data-test', 'view-all-link');
+      customLink.href = href;
+      customLink.innerHTML = `<span class="_marketText_1lyag_1 _semibold20_1lyag_246 _text20_1lyag_291">View all</span>`;
+
+      customLink.style.backgroundColor = '#da2013';
+      customLink.style.borderRadius = '6px';
+      customLink.style.paddingLeft = '6px';
+
+      customLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        history.pushState({}, '', href);
+        window.dispatchEvent(new PopStateEvent('popstate'));
+      });
+
+      return customLink;
+    }
+
+    new MutationObserver(insertLinks).observe(document.body, { childList: true, subtree: true });
+  }
+
+  window.addEventListener('load', addCustomLinks);
 })();
